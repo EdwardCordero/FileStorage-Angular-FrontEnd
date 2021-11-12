04b9cfbbe5f6174a2ffac1260dcc0269c85891e0
Oscar Maldonado <oscarmaldonado@Oscars-MacBook-Pro.local>
2021-11-11 17:41:04 -0800

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ComponentComponent } from './component/component.component';
+import { LoginComponentsComponent } from './components/login-components/login-components.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ComponentComponent,
+    LoginComponentsComponent
   ],
   imports: [
     BrowserModule,
